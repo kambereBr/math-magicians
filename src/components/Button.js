2ef4d6badb1b477f54f15835a92d4ad6eb923188
Bruno Kambere <brunokambere@gmail.com>
2024-01-01 23:37:11 +0200

    Refactor Button component props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ content, btnClassName = 'gray-button', clickEvent }) => (
+const Button = ({ content, btnClassName, clickEvent }) => (
   <button type="button" className={btnClassName} onClick={clickEvent}>{content}</button>
 );
 
 Button.propTypes = {
   content: PropTypes.string.isRequired,
-  btnClassName: PropTypes.string.isRequired,
+  btnClassName: PropTypes.string,
   clickEvent: PropTypes.func.isRequired,
+};
+
+Button.defaultProps = {
+  btnClassName: 'gray-button',
 };
 
 export default Button;
